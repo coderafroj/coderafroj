385e32fcc901c630a679869c9b2b0cee1391915d
Coderafroj <coderafroj@gmail.com>
2026-02-05 15:55:31 +0530

    Update note:

--- src/data/notes/python.js
+++ src/data/notes/python.js
@@ -1,1 +1,14 @@
-export const python = [];
+export const python = [        {
+        id: '',
+        slug: '',
+        title: '',
+        description: '',
+        tags: [''],
+        category: 'Python Masterclass',
+        image: '',
+        createdAt: new Date('2026-02-05'),
+        content: `
+1.2 Wireless Ad-hoc and Sensor Networks: The Ancestors without IP Wireless sensor networks (WSNs) were an emerging application field of microelectronics and communications in the first decade of the twenty-first century. In particular, WSNs promised wide support of interactions between people and their surroundings. The potential of a WSN can be seen in the three words behind the acronym: • “Wireless” puts the focus on the freedom that the elimination of wires gives, in terms of mobility support and ease of system deployment; • “Sensor” reflects the capability of sensing technology to provide the means to perceive and interact — in a wide sense — with the world; • “Networks” gives emphasis to the possibility of building systems whose functional capabilities are given by a plurality of communicating devices, possibly distributed over large areas.
+`
+    },
+];
