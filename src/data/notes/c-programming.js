1326833587f0827ca9f3135af880803fa8e9c1b5
Coderafroj <coderafroj@gmail.com>
2026-02-07 09:41:03 +0530

    Update note: tuple

--- src/data/notes/c-programming.js
+++ src/data/notes/c-programming.js
@@ -1,1 +1,14 @@
-export const cProgramming = [];
+export const cProgramming = [        {
+        id: 'tuple',
+        slug: 'tuple',
+        title: 'tuple',
+        description: '',
+        tags: [''],
+        category: 'C Programming',
+        image: '',
+        createdAt: new Date('2026-02-07'),
+        content: `
+**tuple** i sa mutble data tyep
+`
+    },
+];
