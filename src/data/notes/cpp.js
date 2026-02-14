c064ccaa39e556110f57569c98711630cd9633c1
Coderafroj <coderafroj@gmail.com>
2026-02-14 09:18:21 +0530

    Update note:

--- src/data/notes/cpp.js
+++ src/data/notes/cpp.js
@@ -1,1 +1,18 @@
-export const cpp = [];
+export const cpp = [        {
+        id: '',
+        slug: '',
+        title: '',
+        description: '',
+        tags: [''],
+        category: 'C++ Systems Programming',
+        image: '',
+        createdAt: new Date('2026-02-14'),
+        content: `
+My name is afroj
+
+<table style="min-width: 75px;">
+<colgroup><col style="min-width: 25px;"><col style="min-width: 25px;"><col style="min-width: 25px;"></colgroup><tbody><tr><th colspan="1" rowspan="1"><p>Nn</p><p>S</p><p></p></th><th colspan="1" rowspan="1"><p></p></th><th colspan="1" rowspan="1"><p></p></th></tr><tr><td colspan="1" rowspan="1"><p></p></td><td colspan="1" rowspan="1"><p></p></td><td colspan="1" rowspan="1"><p></p></td></tr><tr><td colspan="1" rowspan="1"><p></p></td><td colspan="1" rowspan="1"><p></p></td><td colspan="1" rowspan="1"><p></p></td></tr></tbody>
+</table>
+`
+    },
+];
