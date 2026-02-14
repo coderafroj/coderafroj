1e6d1713ae25efc40c75a7645ee6c9f4c0f03d73
Coderafroj <coderafroj@gmail.com>
2026-02-14 09:20:02 +0530

    Update note: Tuple

--- src/data/notes/c-programming.js
+++ src/data/notes/c-programming.js
@@ -1,12 +1,12 @@
-export const cProgramming = [        {
+export const cProgramming = [            {
         id: 'tuple',
         slug: 'tuple',
-        title: 'tuple',
-        description: '',
-        tags: [''],
+        title: 'Tuple',
+        description: 'List is important data type in python ',
+        tags: ['List'],
         category: 'C Programming',
         image: '',
-        createdAt: new Date('2026-02-07'),
+        createdAt: new Date('Sat Feb 07 2026 05:30:00 GMT+0530 (India Standard Time)'),
         content: `
 **tuple** i sa mutble data tyep
 `
